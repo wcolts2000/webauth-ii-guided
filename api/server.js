82fcc5dc843320597e4a62ad4389fa3a8eddbbd7
wcolts2000 <wcolts2000@gmail.com>
2019-05-21 13:31:42 -0700

    solution for guided project complete with database session storage used

--- api/server.js
+++ api/server.js
@@ -1,21 +1,60 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const KnexSessionsStore = require("connect-session-knex")(session);
 
 const server = express();
+
+const sessionConfig = {
+  name: "monster", // defaults to sid, but we should change this as that clues in on our stack
+  secret: "keep it safe, keep it secret -gandalf", // used to encrypt/decrypt the cookie (use a .env file here)
+  cookie: {
+    maxAge: 1000 * 60 * 60, // (this is 1 hr) how long the session is valid for in millisec
+    secure: false // used for https only communications, should be true in production
+  },
+  httpOnly: true, // cannot access the cookie from JS using document.cookie
+  // keep this to true unless you have a very good reason to not have it as true
+  resave: false, // keep it false to avoid recreating sessions that have not changed
+  saveUninitialized: false, // GDPR Laws against setting cookies automatically
+
+  // we add this to configure the way sessions are stored
+  store: new KnexSessionsStore({
+    knex: require("../database/dbConfig"), // configures the instance of knex
+    tablename: "sessions", // table that will store sessions inside the db, name it anything we want
+    sidfieldname: "sid", // column that will hold the session id, name this whatever we want
+    createtable: true, // if the table does not exist, it will be created automatically for us
+    clearInterval: 1000 * 60 * 60 //the amount of time before checking old sessions and removing them from te database to keep everything clean and performant
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
 module.exports = server;
+
+// resulting database entry for the  session
+// {"cookie": {
+//     "originalMaxAge":3600000,
+//     "expires":"2019-05-21T18:43:33.664Z",
+//     "secure":false,
+//     "httpOnly":true,
+//     "path":"/"
+//   },
+//   "user": {
+//     "id":1,
+//     "username":"sam",
+//     "password":"$2a$10$AYYoF245l8SAxu7./x40fuOngXfXpDtW6PwmC9xs1MW9k1ZJRrLAy"
+//   }
+// }
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +18,44 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session is an object that is added by the sessions middleware
+        req.session.user = user;
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    // the library exposes the destroy method that will remove the session for the client
+    req.session.destroy(err => {
+      if (err) {
+        res.send(
+          "you can checkout any time you like, but you can never leave..."
+        );
+      } else {
+        res.send("bye, thanks for playing");
+      }
+    });
+  } else {
+    // if there is no session, just end the request or send a response
+    // we chose to just end the response for this example
+    res.end();
+  }
 });
 
 module.exports = router;
